c6cf996e4f762fa96155b958e2d914a5ad3252bd
parkjongtaek <lovedisket@naver.com>
2020-07-10 16:43:23 +0900

    img js inc blade html 푸시

---
+++ blog/public/js/index.js
@@ -0,0 +1,183 @@
+$(function(){
+    var nav = $('.nav_list li');
+    var sub_nav = $('.sub_nav');
+    var ham_list = $('.ham_list');
+    var nav_list,
+        nav_offset,
+        ham_nav = "";
+    $(sub_nav).hide();
+    $(".hamburger").click(function(){
+        
+        $('#ham_nav').toggle();
+      });
+    function on_sub_nav(nav_list,nav_offset){
+        $(sub_nav).hide();
+        $('.'+nav_list).show();
+        var ul_width = $('.'+nav_list+' ul').width();
+        //console.log(ul_width);
+        if(nav_list == "Contact"){
+            $('.'+nav_list+' ul').eq(0).css({marginLeft:nav_offset-ul_width+"px"});
+        }else{
+            $('.'+nav_list+' ul').eq(0).css({marginLeft:nav_offset-ul_width/2+"px"});
+        }
+        
+    }
+    $(nav,sub_nav).hover(function(){
+        nav_list = $(this).text(); 
+        nav_offset = $(this).offset().left;
+        on_sub_nav(nav_list,nav_offset)
+    });
+    $(sub_nav).hover(function(){
+        
+    },function(){
+        $(sub_nav).hide();
+    });
+
+    $(ham_list).click(function(){
+        ham_nav = $(this).text();
+        $('.ham'+ham_nav+'').slideToggle();
+    })
+    //서브
+
+    //material
+    $("#sub_sec .material_sub_img").click(function(){
+        var this_con = $(this);
+        var idx = $(this).parent().index();
+
+        function doing(){
+            var header_height = $("#header").height();
+            var this_con_offset = $(this_con).offset().top;
+            $('html, body').animate({scrollTop:this_con_offset-header_height},0);
+        }
+
+        var start_inter = setInterval(doing,0);
+
+        function even_odd(check_idx){
+            return (check_idx % 2)? "odd":"even";
+        }
+        var even_or_odd = even_odd(idx)
+        console.log(even_or_odd)
+        $(this).stop();
+        $("#sub_sec .material_sub_img").removeClass('on_')
+        $(this_con).addClass('on_');
+        for(var i=0; i<$('.material_sub').length; i++){
+            $("#sub_sec .material_sub_img").eq(i).not(".on_").children('img').attr('src','../img/sub/material_0'+(i+1)+'.png');
+            $("#sub_sec .material_sub_img").eq(i).not(".on_").children('div').css({
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%,-50%)"
+            });
+            $("#sub_sec .material_sub_img").not(".on_").eq(i).children('div').children('h2').css({
+                position: "relative",
+                top: "auto",
+                bottom: "auto",
+                left: "auto",
+                right: "auto",
+                transform: "translate(0)"
+            });
+            $("#sub_sec .material_sub_img").not(".on_").eq(i).children('div'). children('p').css({
+                position: "relative",
+                top: "auto",
+                bottom: "auto",
+                left: "auto",
+                right: "auto",
+                transform: "translate(0)",
+                textAlign: "center"
+            });
+        }
+		if($(this_con).hasClass('on') == false){
+            
+            $(this_con).children('img').fadeOut(0);
+            
+            $(this_con).children('img').attr('src','../img/sub/material_0'+(idx+1)+'_on.png').fadeIn(500);;
+            setTimeout(function(){
+                var img_height = $(this_con).children('img').height();
+            
+            if(even_or_odd == "even"){
+                console.log(img_height)
+                console.log($(this_con))
+                $(this_con).children('div').css({
+                    height:img_height+"px",
+                    position:"absolute",
+                    top:0,
+                    left:0,
+                    transform:"translate(0)"
+                })
+                $(this_con).children('div').children('h2').css({
+                    position:"absolute",
+                    top:"15%",
+                    bottom:"auto",
+                    left:"50%"
+                }).animate({
+                    top:"15%",
+                    bottom:"auto",
+                    left:"13.5417vw"
+                },700);
+                $(this_con).children('div').children('p.toggle_text').css({
+                    textAlign:"right",
+                    position:"absolute",
+                    top:"auto",
+                    bottom:"6.9444vw",
+                }).animate({
+                    top:"auto",
+                    bottom:"6.9444vw",
+                    left:"auto",
+                    right:"13.5417vw"
+                },1000);
+            }else{
+                $(this_con).children('div').css({
+                    height:img_height+"px",
+                    position:"absolute",
+                    top:0,
+                    left:0,
+                    transform:"translate(0)"
+                })
+                $(this_con).children('div').children('h2').css({
+                    position:"absolute",
+                    top:"15%",
+                    bottom:"auto",
+                    left:"auto"
+                }).animate({
+                    top:"15%",
+                    bottom:"auto",
+                    right:"13.5417vw"
+                },700);
+                $(this_con).children('div').children('p.toggle_text').css({
+                    textAlign:"left",
+                    position:"absolute",
+                    top:"auto",
+                    bottom:"6.9444vw",
+                }).animate({
+                    top:"auto",
+                    bottom:"6.9444vw",
+                    right:"auto",
+                    left:"13.5417vw"
+                },1200);
+            }
+        },200)
+            setTimeout(function(){
+                $(this_con).animate({height:$(this_con).children('img').height()-5},700);
+            },200);
+        }
+        setTimeout(function(){
+            clearInterval(start_inter);
+        },500);
+        $("#sub_sec .material_sub_img").removeClass("on");
+        $(this_con).toggleClass("on");
+        
+        $("#sub_sec .material_sub_img").not('.on').animate({height:"15.6250vw"},0);
+        $("#sub_sec .material_sub_img").not('.on').children('div').css({height:"auto"});
+    });
+
+    //news
+    $('.news_tabs a').click(function(){
+        var idx = $(this).index();
+        $('.news_tabs a').removeClass('on');
+        $(this).addClass('on');
+        $('.news_content ul.news_tabs_').hide();
+        $('.news_content ul.news_tabs_').eq(idx).show();
+        $('.pag_write').hide();
+        $('.pag_write').eq(idx).show();
+    })
+});
---
+++ blog/resources/views/sub/heritage01.blade.php
@@ -0,0 +1,91 @@
+<?php
+	include_once $_SERVER["DOCUMENT_ROOT"]."/main/inc/head.html";
+?>
+			<div id="sub_sec">
+                <div class="heritage01_sub_01">
+                    <!-- <div class="back_path">
+                        <div class="path_01 path_child">
+                            <img class="aaa" src="../img/sub/path1.png" alt="">
+                            <img src="../img/sub/path1.png" alt="">
+                        </div>
+                        <div class="path_02 path_child">
+                            <img src="../img/sub/path1.png" alt="">
+                            <img src="../img/sub/path1.png" alt="">
+                        </div>
+                        <div class="path_03 path_child">
+                            <img src="../img/sub/path1.png" alt="">
+                            <img src="../img/sub/path1.png" alt="">
+                        </div>
+                    </div> -->
+                    <div class="h_sub_top">
+                        <div class="h_sub_text_box">
+                            <h1 class="hei85p">PERFECT<br>SLEEP TAILORED<br>FOR YOU SINCE 1852</h1>
+                            <p><span class="font_noto">Hästens</span>는 창립자인<br>페르 아돌프 얀슨(<span class="font_noto">Pehr Adolf Janson</span>)은<br>스웨덴 국왕으로부터 인정받은<br>고급 말 안장을 제작하던 '장인'이었습니다.</p>
+                        </div>
+                        <div class="h_sub_img_box">
+                            <img src="../img/sub/spirit_01.png" alt="">
+                        </div>
+                    </div>
+                    <div class="h_sub_bot">
+                        <div class="h_sub_img_box">
+                            <img src="../img/sub/spirit_02.png" alt="">
+                        </div>
+                        <div class="h_sub_text_box">
+                            <h1>편안하고 내구성이<br>뛰어난 침대를<br>탄생시켰습니다.</h1>
+                            <p>해스텐스(<span class="font_noto">Hästens</span>)의 역사는<br>그레이엄 벨과 토마스 에디슨이 그들의 발명품인<br>전화기와 전구로 사람들의 눈과 귀를 밝히기도 전인<br>1852년으로 거슬러 올라갑니다.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="heritage01_sub_02">
+                    <div class="h_sub_bot1">
+                        <p>
+                            본래 해스텐스는 고급 말 안장 제작 기업으로 시작되었는데,<br>
+                            당시에는 안장 제작자가 침대 매트리스도 함께 만들었기에, 해스텐스 역시 안장과 매트리스를 함께 제작하였습니다.<br>
+                            그 후 전문 침대 제작 기업으로 성장시키기 위해 수공예 마스터 기술을 연마하고 발전시켰습니다.<br>
+                            해스텐스는 북유럽 스웨덴의 독특한 감각을 결합해 침대 제조 기술을 한 단계 향상시켰고,<br>
+                            더욱 편안하고 내구성이 뛰어난 침대를 탄생시켰습니다.
+                        </p>
+                    </div>
+                    <div class="h_sub_bot2">
+                        <div class="h_sub_bot_text">
+                            <h1>소재와 품질에 대한 고집</h1>
+                            <p>
+                                1953년 무렵 해스텐스는 미국과 스웨덴을 오가던 <span class="font_noto">SAL</span> 고급 크루즈 유람선에 침대를 공급하게 되었고,<br>
+                                이후부터 해스텐스 침대의 놀라운 품질은 전 세계로 뻗어 나가게 되었습니다.<br>
+                                해스텐스는 1852년부터 지금까지 오직 인간에게 최상의 숙면을 제공하기 위한 하이엔드 침대의 제작에 초점을 맞춰왔으며,<br>
+                                소재와 품질에 대한 고집은 자연스럽게 "세계 최고의 침대"로 모두가 공인하는 브랜드로 자리매김하였습니다.<br><br>
+                                해스텐스가 설립되었을 당시에는 기존의 침대보다 더 좋은 것을 만드는 것과<br>
+                                침대 제조 기술을 연마하여 재창조 하는 것이 주목적이었습니다.<br>
+                                이에 초기부터 모든 침대에 홀스헤어, 울코튼 플렉스(아마 섬유)와 같은 최고의 천연 소재만을 침대의 충전재로 사용하였습니다.<br>
+                                이런 장인 정신이 담긴 전통은 천연 소재에 대한 이해와 재료의 효능을 극대화하는 밑거름이 되었습니다.<br><br>
+                                당신은 우리의 영감입니다.<br>
+                                최고의 수면을 취하기 위해 필요한 것과 서비스, 건축, 품질, 편안함 및 기능 모든것이 중요합니다. <br>
+                                6대에 걸친 수면 공예와 천연 재료가 절정에 있습니다. 당신은 우리의 끝없는 우수성을 추구하면서 항상 우리를 이끌어 왔습니다.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+<?php
+	include_once $_SERVER["DOCUMENT_ROOT"]."/main/inc/footer.html";
+?>
+        <script>
+             $(document).ready(function(){
+   console.log("a")
+});
+            var swiper = new Swiper('.main_slider', {
+                direction: 'vertical',
+                spaceBetween: 10,
+                loop: true,
+                autoplay: {
+                    delay: 1800,
+                    disableOnInteraction :false,
+                },
+                pagination: {
+                  el: '.swiper-pagination',
+                  clickable: true,
+                },
+            });
+        </script>
+    </body>
+</html>
