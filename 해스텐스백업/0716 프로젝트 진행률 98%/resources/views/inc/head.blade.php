b02b0ccb40df09e8f56c2b4286b9933728f7c661
kangsan-ks <to123dn@gmail.com>
2020-07-16 11:06:15 +0900

    진행률 98% 백업파일

---
+++ 해스텐스백업/0716 프로젝트 진행률 98%/resources/views/inc/head.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <!-- <link href="//fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap" rel="stylesheet"> -->
+        <link href="//fonts.googleapis.com/css?family=Noto+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/index.css">
+        <link rel="stylesheet" href="/css/swiper.min.css">
+        <script src="https://kit.fontawesome.com/7f5faa19ba.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="/js/index.js"></script>
+        <script src="/js/swiper.min.js"></script>
+    </head>
+@include('inc/header')
